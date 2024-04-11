3a2760cd6a13c9214bda145a0d2a3e440fa52d78
GabrielaOliTavares <145030166+GabrielaOliTavares@users.noreply.github.com>
2024-04-11 17:37:10 +0000

    $git commit

---
+++ questao19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Atividade19 {
+    public static void main(String[] args) {
+numero=int(input("Digite um numero:"))
+while numero>=1000:
+  print("O numero precisa ser menor que mil")
+  numero=int(input("Digite um numero:"))
+numero=str(numero)
+numero=numero[::-1]
+lista=["unidade","dezena","centena"]
+for i, num in enumerate(numero):
+  print(num+" é "+lista[i])
+print("Fim do programa")
+
+    }
+}
